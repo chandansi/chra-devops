ea6c5974638be63076f7b48725723bc7dbbd6c61
Yatharth Rai <chandan.yathu@gmail.com>
2025-12-13 00:23:05 +0530

    bacend

---
+++ environments/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    # resource_group_name  = "ch_rg"
+    # storage_account_name = "chatbotstgacc"
+    # container_name       = "tfstate"
+    # key                  = "dev.backend.tfstate"
+  }  
+  
+}
--- environments/dev/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  rg_name     = "vm-rg0107"
-  rg_location = "centralindia"
-}
