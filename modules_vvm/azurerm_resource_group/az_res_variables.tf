cd4ef15b76d893ced7230fad0f1c07bb19cd7da5
chandansi <105107325+chandansi@users.noreply.github.com>
2025-12-13 23:49:31 +0530

    Update az_res_variables.tf

--- modules_vvm/azurerm_resource_group/az_res_variables.tf
+++ modules_vvm/azurerm_resource_group/az_res_variables.tf
@@ -3,24 +3,6 @@ variable "vm_rgss" {
     type = map(object({
         rg_name                       = string
         location                      = string
-        vnet_name                     = string
-        vnet_address_space            = list(string)
-        vnet_location                 = string
-        subnet_name                   = string
-        subnet_address_prefixes       = list(string)
-        nic_name                      = string
-        nic_location                  = string
-        ip_configuration_name         = string
-        subnet_id                     = string
-        private_ip_address_allocation = string
-        vm_name                       = string
-        admin_ssh_key_username        = string
-        admin_ssh_key_public_key      = string
-        os_disk_caching               = string
-        os_disk_storage_account_type  = string
-        source_image_publisher        = string
-        source_image_offer            = string  
-        source_image_sku              = string
-        source_image_version          = string
+      
     }))
 }
