0c601c07dd21eb24d5f6dfe23d4cd26894b5f4f4
chandansi <105107325+chandansi@users.noreply.github.com>
2025-12-13 15:56:17 +0530

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 terraform {
   backend "azurerm" {
     resource_group_name  = "ch_rg01"
@@ -8,14 +8,4 @@ terraform {
   }  
   
 }
-=======
- terraform {
-  backend "azurerm" {
-    # resource_group_name  = "ch_rg01"
-    # storage_account_name = "vmstg007"
-    # container_name       = "vmcontainer07"
-    # key                  = "infra.tfstate"
-   }  
-  
- }
->>>>>>> 44832c75df7082e36abdcd12598cf7f90a840f0e
+
